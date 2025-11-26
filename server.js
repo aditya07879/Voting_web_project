3b28cbd9eb8c92b33817374334bcc45c4591c6be
aditya07879 <aditya07879@gmail.com>
2025-11-26 20:55:08 +0530

    Add views for voting app

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const express = require(express)
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (res,res) => {
-    res.render("hello")
-})
+// Correct view engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
 
-app.listen(3000);
+// Correct route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/candidates', (req, res) => res.render('candidates'));
+app.get('/results', (req, res) => res.render('results'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/profile', (req, res) => res.render('profile'));
+app.get('/admin', (req, res) => res.render('admin-dashboard'));
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
